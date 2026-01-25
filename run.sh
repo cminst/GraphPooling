dda3f7dfa4855cf396da00f232f2dcb1b60bd8a4
Qingyun Li <qingy2019@outlook.com>
2026-01-25 11:57:57 -0800

    Set2Set results

---
+++ run.sh
@@ -0,0 +1,6 @@
+python3 gnn_pooling_benchmark_cv.py --dataset REDDIT-BINARY --model Set2Set --tune --seed 43
+python3 gnn_pooling_benchmark_cv.py --dataset REDDIT-BINARY --model Set2Set --tune --seed 44
+python3 gnn_pooling_benchmark_cv.py --dataset REDDIT-BINARY --model GCN --tune --seed 43
+python3 gnn_pooling_benchmark_cv.py --dataset REDDIT-BINARY --model GCN --tune --seed 44
+python3 gnn_pooling_benchmark_cv.py --dataset REDDIT-BINARY --model TopKPool --tune --seed 43
+python3 gnn_pooling_benchmark_cv.py --dataset REDDIT-BINARY --model TopKPool --tune --seed 44
