ce4ce51c9e39f0773d029c44089148eb72315e7c
Qingyun Li <qingy2019@outlook.com>
2026-01-25 12:52:51 -0800

    updates

--- run.sh
+++ run.sh
@@ -1,6 +1,12 @@
-python3 gnn_pooling_benchmark_cv.py --dataset REDDIT-BINARY --model Set2Set --tune --seed 43
-python3 gnn_pooling_benchmark_cv.py --dataset REDDIT-BINARY --model Set2Set --tune --seed 44
-python3 gnn_pooling_benchmark_cv.py --dataset REDDIT-BINARY --model GCN --tune --seed 43
-python3 gnn_pooling_benchmark_cv.py --dataset REDDIT-BINARY --model GCN --tune --seed 44
-python3 gnn_pooling_benchmark_cv.py --dataset REDDIT-BINARY --model TopKPool --tune --seed 43
-python3 gnn_pooling_benchmark_cv.py --dataset REDDIT-BINARY --model TopKPool --tune --seed 44
+#   --model MODEL         Model name: TopKPool, SAGPool, ASAP, EdgePool, MinCutPool, DiffPool, Graclus, GMT, Global-Attention, SortPool, Set2Set, GCN, GIN
+
+python3 gnn_pooling_benchmark_cv.py --dataset REDDIT-BINARY --model SAGPool --tune --seed 43
+python3 gnn_pooling_benchmark_cv.py --dataset REDDIT-BINARY --model SAGPool --tune --seed 44
+python3 gnn_pooling_benchmark_cv.py --dataset REDDIT-BINARY --model DiffPool --tune --seed 42
+python3 gnn_pooling_benchmark_cv.py --dataset REDDIT-BINARY --model DiffPool --tune --seed 43
+python3 gnn_pooling_benchmark_cv.py --dataset REDDIT-BINARY --model DiffPool --tune --seed 44
+python3 gnn_pooling_benchmark_cv.py --dataset REDDIT-BINARY --model Graclus --tune --seed 42
+python3 gnn_pooling_benchmark_cv.py --dataset REDDIT-BINARY --model Graclus --tune --seed 43
+python3 gnn_pooling_benchmark_cv.py --dataset REDDIT-BINARY --model Graclus --tune --seed 44
+
+
