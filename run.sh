819afd5cbd73a862a0c7259821b746f1c9f81cba
Qingyun Li <qingy2019@outlook.com>
2026-01-25 20:07:49 -0800

    results

--- run.sh
+++ run.sh
@@ -1,9 +1,9 @@
 #   --model MODEL         Model name: TopKPool, SAGPool, ASAP, EdgePool, MinCutPool, DiffPool, Graclus, GMT, Global-Attention, SortPool, Set2Set, GCN, GIN
 
-python3 gnn_pooling_benchmark_cv.py --dataset REDDIT-BINARY --model GMT --tune --seed 42
-python3 gnn_pooling_benchmark_cv.py --dataset REDDIT-BINARY --model GMT --tune --seed 43
-python3 gnn_pooling_benchmark_cv.py --dataset REDDIT-BINARY --model GMT --tune --seed 44
+python3 gnn_pooling_benchmark_cv.py --dataset REDDIT-BINARY --model GMT --tune --seed 45
+python3 gnn_pooling_benchmark_cv.py --dataset REDDIT-BINARY --model GMT --tune --seed 46
+python3 gnn_pooling_benchmark_cv.py --dataset REDDIT-BINARY --model GMT --tune --seed 47
 
-python3 gnn_pooling_benchmark_cv.py --dataset REDDIT-BINARY --model Global-Attention --tune --seed 42
-python3 gnn_pooling_benchmark_cv.py --dataset REDDIT-BINARY --model Global-Attention --tune --seed 43
-python3 gnn_pooling_benchmark_cv.py --dataset REDDIT-BINARY --model Global-Attention --tune --seed 44
+python3 gnn_pooling_benchmark_cv.py --dataset REDDIT-BINARY --model Global-Attention --tune --seed 45
+python3 gnn_pooling_benchmark_cv.py --dataset REDDIT-BINARY --model Global-Attention --tune --seed 46
+python3 gnn_pooling_benchmark_cv.py --dataset REDDIT-BINARY --model Global-Attention --tune --seed 47
